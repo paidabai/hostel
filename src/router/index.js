affd8865f386f19b4089ab1a7cfd5494eedd7566
991519324@qq.com <11584270+pu-siyou@user.noreply.gitee.com>
2022-11-16 16:33:19 +0800

    在 router-index.js 中添加方法处理 路由重复跳转的问题。

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,13 @@ import ContactHeadquarters from '../views/ContactHeadquarters/index.vue'
 import latestNews from '../views/LatestNews/index.vue'
 Vue.use(VueRouter)
 
+// 获取原型对象上的push函数
+const originalPush = VueRouter.prototype.push
+// 修改原型对象中的push方法
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
 const router = new VueRouter({
   mode: 'history',
   base: import.meta.env.BASE_URL,
