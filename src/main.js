c333516bd3c0991af125b8ba49659d0c90fcd145
paidabai <1196270151@qq.com>
2022-11-25 22:15:20 +0800

    引入input组件

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import App from './App.vue'
 import router from './router'
 import store from "./store";
 import VueScrollTo from 'vue-scrollto'
-import { Loading, Message, Select, Option }from 'element-ui';
+import { Loading, Message, Select, Option, Input }from 'element-ui';
 import Ripple from 'vue-ripple-directive'
 import 'element-ui/lib/theme-chalk/index.css'
 import "element-ui/lib/select";
 import "element-ui/lib/option";
 import "element-ui/lib/button";
-import "element-ui/lib/form"
+import "element-ui/lib/form";
+import "element-ui/lib/input";
 import './assets/css/initialize.css'
 import SIdentify from './components/Form/identify.vue'
 import './assets/css/initialize.css'
@@ -37,10 +38,9 @@ let options = {
 Vue.use(Loading.directive);
 Vue.use(VueScrollTo, options);
 Vue.directive('ripple', Ripple);
-// Vue.component(Select.name, Select);
-// Vue.component(Option.name, Option);
 Vue.use(Select)
 Vue.use(Option)
+Vue.use(Input)
 // 验证
 Vue.component('s-identify', SIdentify)
 
